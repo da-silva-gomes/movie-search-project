9db12de548678d2fd63fc107c7cd2c630d4d9ef1
José Gomes <j.da.silva8.gomes@gmail.com>
2025-04-10 17:22:23 +0100

    initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="navbar-wrapper">
+    <div class="navbar-content">
+      <h3 class="logo">
+        <slot name="heading"></slot>
+      </h3>
+      <div>
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped />
